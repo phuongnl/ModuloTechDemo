8813e836c774d6ab77af15461567e586076cce6b
Phuong Nguyen <phuongnl.hn@gmail.com>
2021-07-27 13:53:17 +0700

    Initial Commit

---
+++ ModuloTech/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ModuloTech
+//
+//  Created by Lucas Lee on 27/07/2021.
+//  Copyright © 2021 Lucas Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
